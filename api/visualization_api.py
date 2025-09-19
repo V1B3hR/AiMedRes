2abf910fdecffff959bc3e188fc24cb3d2728c74
V1B3hR <83901968+V1B3hR@users.noreply.github.com>
2025-09-19 20:56:52 +0100

    Fix logger initialization formatting
    Signed-off-by: V1B3hR <83901968+V1B3hR@users.noreply.github.com>

--- api/visualization_api.py
+++ api/visualization_api.py
@@ -20,7 +20,7 @@ import os
 
 # Import our monitoring systems
 import logging
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__) 
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
