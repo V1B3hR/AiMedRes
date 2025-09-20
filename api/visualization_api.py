f78159d5bdc4695a8bf8f568229fc391a4a821ac
V1B3hR <83901968+V1B3hR@users.noreply.github.com>
2025-09-20 06:40:35 +0100

    Update logger name for visualization API
    Signed-off-by: V1B3hR <83901968+V1B3hR@users.noreply.github.com>

--- api/visualization_api.py
+++ api/visualization_api.py
@@ -20,7 +20,9 @@ import os
 
 # Import our monitoring systems
 import logging
-logger = logging.getLogger(__name__) 
+logger = logging.getLogger(__name__)
+...
+logger = logging.getLogger('duetmind.visualization.api')
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
